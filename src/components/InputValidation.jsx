a2e64f399f2f4df9eba9056ac80d7b49fa3f3747
Azalya Rahmatika <azalyarahmatika@gmail.com>
2024-08-27 23:50:04 +0300

    adding form validation

---
+++ src/components/InputValidation.jsx
@@ -0,0 +1,7 @@
+function InputValidation() {
+  return (
+    <p className="text-red text-xs mt-2">This field is required</p>
+  )
+}
+
+export default InputValidation;
--- src/components/MortgageForm.jsx
+++ src/components/MortgageForm.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import ButtonCalculate from "./ButtonCalculate";
+import InputValidation from './InputValidation';
 
 function MortgageForm({ entryData }) {
   const [mortgageAmount, setMortgageAmount] = useState('');
   const [mortgageTerm, setMortgageTerm] = useState('');
   const [interestRate, setInterestRate] = useState('');
   const [mortgageType, setMortgageType] = useState('');
+  const [errorMortgageAmount, setErrorMortgageAmount] = useState(false);
+  const [errorMortgageTerm, setErrorMortgageTerm] = useState(false);
+  const [errorInterestRate, setErrorInterestRate] = useState(false);
+  const [errorMortgageType, setErrorMortgageType] = useState(false);
 
   const formatNumber = (value) => {
     if (value === '') return '';
@@ -40,6 +45,40 @@ function MortgageForm({ entryData }) {
       mortgageType,
     };
 
+    let hasError = false;
+
+    if (formData.mortgageAmount === '' || isNaN(formData.mortgageAmount)) {
+      setErrorMortgageAmount(true);
+      hasError = true;
+    } else {
+      setErrorMortgageAmount(false);
+    }
+  
+    if (formData.mortgageTerm === '' || isNaN(formData.mortgageTerm)) {
+      setErrorMortgageTerm(true);
+      hasError = true;
+    } else {
+      setErrorMortgageTerm(false);
+    }
+  
+    if (formData.interestRate === '' || isNaN(formData.interestRate)) {
+      setErrorInterestRate(true);
+      hasError = true;
+    } else {
+      setErrorInterestRate(false);
+    }
+  
+    if (!formData.mortgageType) {
+      setErrorMortgageType(true);
+      hasError = true;
+    } else {
+      setErrorMortgageType(false);
+    }
+  
+    if (hasError) {
+      return;
+    }
+
     entryData(formData);
   };
 
@@ -49,6 +88,10 @@ function MortgageForm({ entryData }) {
     setInterestRate('');
     setMortgageType('');
     entryData('');
+    setErrorMortgageAmount(false);
+    setErrorMortgageTerm(false)
+    setErrorInterestRate(false);
+    setErrorMortgageType(false);
   };
 
   return (
@@ -60,44 +103,56 @@ function MortgageForm({ entryData }) {
 
       <form className="font-jakarta-plus my-4" onSubmit={handleSubmit}>
         <label htmlFor="mortgage_amount" className="text-sm text-slate_500">Mortgage Amount</label>
-        <div className="relative rounded-md overflow-hidden mt-2 mb-4 border border-slate-500 focus-within:border-lime group">
-          <span className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-slate-300 px-3 h-full flex items-center text-slate-700 text-sm group-focus-within:bg-lime">₤</span>
-          <input 
-            id="mortgage_amount" 
-            type="text" 
-            className="peer pl-12 py-[6px] w-full rounded-md focus:outline-none text-sm font-bold"
-            value={mortgageAmount}
-            onChange={handleMortgageAmountChange}
-          />
+        <div className='mb-4'>
+          <div className="relative rounded-md overflow-hidden mt-2 border border-slate-500 focus-within:border-lime group">
+            <span className="absolute left-0 top-0 bg-slate-300 px-3 h-9 flex items-center text-slate-700 text-sm group-focus-within:bg-lime">₤</span>
+            <input 
+              id="mortgage_amount" 
+              type="text" 
+              className="peer pl-12 py-[6px] w-full rounded-md focus:outline-none text-sm font-bold h-9"
+              value={mortgageAmount}
+              onChange={handleMortgageAmountChange}
+            />
+          </div>
+          {errorMortgageAmount ? <InputValidation />: '' }
         </div>
+        
 
-        <div className="lg:flex lg:justify-between lg:items-end lg:gap-2">
+        <div className="lg:flex lg:justify-between lg:items-top lg:gap-2">
           <div className="lg:flex-grow">
             <label htmlFor="mortgage_term" className="text-sm text-slate_500">Mortgage Term</label>
-            <div className="relative rounded-md overflow-hidden mt-2 mb-4 border border-slate-500 focus-within:border-lime group">
-              <span className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-slate-300 px-3 h-full flex items-center text-slate-700 text-sm group-focus-within:bg-lime">years</span>
-              <input 
-                id="mortgage_term" 
-                type="number" 
-                className="pl-4 py-[6px] block w-full rounded-md focus:outline-none text-sm font-bold"
-                value={mortgageTerm} 
-                onChange={handleMortgageTermChange}
-              />
+            <div className='mb-4'>
+            <div className={`relative rounded-md overflow-hidden mt-2 border focus-within:border-lime group ${errorMortgageTerm ? 'border-red' : 'border-slate-500'}`}>
+                <span className={`absolute right-0 top-1/2 transform -translate-y-1/2  px-3 h-full flex items-center text-slate-700 text-sm group-focus-within:bg-lime ${errorMortgageTerm ? 'bg-red text-white' : 'bg-slate-300 text-slate-700'}`}>years</span>
+                <input 
+                  id="mortgage_term" 
+                  type="number" 
+                  className="pl-4 py-[6px] block w-full rounded-md focus:outline-none text-sm font-bold"
+                  value={mortgageTerm} 
+                  onChange={handleMortgageTermChange}
+                />
+              </div>
+              {errorMortgageTerm ? <InputValidation />: '' }
             </div>
+              
           </div>
           
           <div className="lg:flex-grow">
             <label htmlFor="interest_rate" className="text-sm text-slate_500 border-slate_500">Interest Rate</label>
-            <div className="relative rounded-md overflow-hidden mt-2 mb-4 border border-slate-500 focus-within:border-lime group">
-              <span className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-slate-300 px-3 h-full flex items-center text-slate-700 text-sm group-focus-within:bg-lime">%</span>
-              <input 
-                id="interest_rate" 
-                type="number" 
-                className="pl-4 py-[6px] block w-full rounded-md focus:outline-none text-sm font-bold"
-                value={interestRate} 
-                onChange={handleInterestRateChange}
-              />
+            <div className='mb-4'>
+              <div className="relative rounded-md overflow-hidden mt-2 border border-slate-500 focus-within:border-lime group">
+                <span className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-slate-300 px-3 h-full flex items-center text-slate-700 text-sm group-focus-within:bg-lime">%</span>
+                <input 
+                  id="interest_rate" 
+                  type="number" 
+                  className="pl-4 py-[6px] block w-full rounded-md focus:outline-none text-sm font-bold"
+                  value={interestRate} 
+                  onChange={handleInterestRateChange}
+                />
+              </div>
+              {errorInterestRate ? <InputValidation />: '' }
             </div>
+            
           </div>
         </div>
       
@@ -129,7 +184,8 @@ function MortgageForm({ entryData }) {
           <div className="absolute inset-0 border border-slate_500 peer-checked:border-lime rounded-md w-full h-full peer-checked:bg-[#FAFAE0]"></div>
           <label htmlFor="interest_only" className="inset-0 text-base text-slate_900 flex items-center font-bold absolute left-0 pl-10">Interest Only</label>
         </div>
-      
+        {errorMortgageType ? <InputValidation />: '' }
+
         <ButtonCalculate />
       </form>
     </>
